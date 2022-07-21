2bfac8d0fc571d1c0b636353d915ec34decdadac
hvlopez <hectorvmlopez@gmail.com>
2022-07-21 13:37:45 -0500

    adding video animations

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,23 +1,31 @@
 import React from "react";
 import styled from "styled-components";
+import { videoAnimations } from "../animation";
+import useScroll from "../hooks/useScroll";
+import {motion} from "framer-motion";
 interface IVideo {}
 
-const Video: React.FC<IVideo> = ({}) => {
+const Video: React.FC<IVideo> = ({ }) => {
+  const[element, controls] = useScroll({});
   return (
-    <Section id="video">
-      <div className="background">
-        <div className="video">
-          <iframe
-            width="960"
-            height="515"
-            src="https://www.youtube.com/embed/l0rpabzkkZE"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </div>
-      </div>
+    <Section id="video" ref={element as any}>
+      <div className="background" />
+      <motion.div
+        variants={videoAnimations}
+        animate={controls as any}
+        transition={{ delay: 0.03, type: "tween", duration: 0.8 }}
+        className="video"
+      >
+        <iframe
+          width="960"
+          height="515"
+          src="https://www.youtube.com/embed/l0rpabzkkZE"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </motion.div>
     </Section>
   );
 };
