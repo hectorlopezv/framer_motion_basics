7731863a4bd223bc56d1722aa1adbb3d5a8de0ae
hvlopez <hectorvmlopez@gmail.com>
2022-07-21 10:57:45 -0500

    video

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,7 +1,37 @@
-import React,  { FC } from "react";
+import React from "react";
+import styled from "styled-components";
 interface IVideo {}
 
-const Video: FC<IVideo> = ({}) => {
-  return <div>Video</div>;
+const Video: React.FC<IVideo> = ({}) => {
+  return (
+    <Section>
+      <div className="background">
+        <div className="video">
+          <iframe
+            width="960"
+            height="515"
+            src="https://www.youtube.com/embed/l0rpabzkkZE"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
+      </div>
+    </Section>
+  );
 };
 export default Video;
+const Section = styled.section`
+  background-color: #662d91aa;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  display:flex;
+  position: relative;
+
+
+  .video {
+    z-index: 10;
+  }
+`;
